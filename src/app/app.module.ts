7ca31316bc60843915a75693525314f6891eb122
vijila mol s s <vijilamol.ss@experionglobal.com>
2019-09-20 10:03:12 +0530

    design completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// @ngmodule object and its properties
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
+// @ng module object and its properties
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    SearchComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
